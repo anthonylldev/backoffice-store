ad31f446078e17d085a33b0c15632fc6a9221337
Anthony Fabian Leon Lucero <anthonyfabian22ll@gmail.com>
2022-05-17 02:08:26 +0200

    Add initial components and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CategoryListComponent} from "./entities/category/category-list/category-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'categories', component: CategoryListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
