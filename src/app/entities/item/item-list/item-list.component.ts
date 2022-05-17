fd7767d38e0e69127460fd33a49a846b84817f6d
Anthony Fabian Leon Lucero <anthonyfabian22ll@gmail.com>
2022-05-17 12:59:42 +0200

    Add simple nav to item

--- src/app/entities/item/item-list/item-list.component.ts
+++ src/app/entities/item/item-list/item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-item-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-list.component.scss']
 })
 export class ItemListComponent implements OnInit {
+  categoryId?: number;
+  title: string = "";
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    if (this.route.snapshot.paramMap.get("categoryId")) {
+      this.categoryId = +this.route.snapshot.paramMap.get("categoryId")!;
+      this.title = "Artículos de la categoría " + this.categoryId;
+    } else {
+      this.title = "Lista de artículos"
+    }
   }
-
 }
